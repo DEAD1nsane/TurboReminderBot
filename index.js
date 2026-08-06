b01c9bc759d39bf9cf55ec8c1941fd4840a81ac7
DEAD1nsane <turbolaceup@gmail.com>
2026-08-06 08:13:30 -0500

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const chrono = require('chrono-node');
+const { DateTime } = require('luxon');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.post('/webhook', (req, res) => {
+    console.log('Received Telegram update:', req.body);
+    res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
