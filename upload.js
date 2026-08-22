1df11d0d1e098ce4ae5d00fdc65ce99bd466c013
DEAD1nsane <turbolaceup@gmail.com>
2026-08-22 02:47:48 -0500

    update upload.js

--- upload.js
+++ upload.js
@@ -27,8 +27,8 @@ const filesToUpload = [
   { localPath: path.join(__dirname, 'telegram.js'), driveName: 'telegram.js.txt', mimeType: 'text/plain' },
   { localPath: expandHome('~/storage/shared/Backups/Termux/.termux.properties.txt'), driveName: 'termux.properties.txt', mimeType: 'text/plain' },
   { localPath: expandHome('~/storage/shared/Backups/Termux/.zshrc.txt'), driveName: 'zshrc.txt', mimeType: 'text/plain' },
-  { localPath: expandHome('~/storage/shared/Backups/Termux/init.lua.txt'), driveName: 'init.lua.txt', mimeType: 'text/plain' },
-  { localPath: expandHome('~/storage/shared/Backups/Termux/darkblood.zsh-theme.txt'), driveName: 'darkblood.zsh-theme.txt', mimeType: 'text/plain' }
+  { localPath: expandHome('~/storage/shared/Backups/Termux/.init.lua.txt'), driveName: 'init.lua.txt', mimeType: 'text/plain' },
+  { localPath: expandHome('~/storage/shared/Backups/Termux/.darkblood.zsh-theme.txt'), driveName: 'darkblood.zsh-theme.txt', mimeType: 'text/plain' }
 ];
 
 async function uploadFiles() {
